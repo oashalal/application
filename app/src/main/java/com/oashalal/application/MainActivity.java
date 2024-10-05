32393bf2200b9acd2a5112c64954228bb632327f
Your Name <you@example.com>
2024-10-05 15:09:50 +0300

    auto

--- app/src/main/java/com/oashalal/application/MainActivity.java
+++ app/src/main/java/com/oashalal/application/MainActivity.java
@@ -1,22 +1,29 @@
 package com.oashalal.application;
 
-import android.os.Bundle;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.oashalal.application.databinding.ActivityMainBinding;
+import android.widget.TextView;
 
-/**
- * MainActivity
- */
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        TextView text = findViewById(R.id.label);
-        text.setText("Hello, World");
-        text.setTextSize(20);
 
-        setContentView(R.layout.activity_main);
+        // Inflate and get instance of binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        // set content view to binding's root
+        setContentView(binding.getRoot());
+        TextView label = findViewById(R.id.label);
+        label.setText("Hello, World");
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.binding = null;
     }
 }
