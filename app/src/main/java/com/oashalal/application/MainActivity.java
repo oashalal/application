cf645c732576d0bec402cee70ef8e082de5fc10e
Your Name <you@example.com>
2024-10-05 15:25:28 +0300

    auto

--- app/src/main/java/com/oashalal/application/MainActivity.java
+++ app/src/main/java/com/oashalal/application/MainActivity.java
@@ -2,11 +2,17 @@ package com.oashalal.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.oashalal.application.databinding.ActivityMainBinding;
+
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private TextView label;
+    private int num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
         // set content view to binding's root
         setContentView(binding.getRoot());
-        TextView label = findViewById(R.id.label);
-        label.setText("Hello, World");
+
+        label = findViewById(R.id.label);
+        Button button = findViewById(R.id.button);
+
+        label.setText("0");
+        label.setTextSize(20);
+        num = 0;
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                label.setText(num++);
+            }            
+        });
+        button.setText("add 1");
     }
     
     @Override
