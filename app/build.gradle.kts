49ec0ae3531810d11d1cec53f629b0ec02c2ab96
Your Name <you@example.com>
2024-10-05 14:24:38 +0300

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+
+plugins {
+    id("com.android.application")
+    
+}
+
+android {
+    namespace = "com.oashalal.application"
+    compileSdk = 33
+    
+    defaultConfig {
+        applicationId = "com.oashalal.application"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        
+    }
+    buildToolsVersion = "33.0.3"
+    
+}
+
+dependencies {
+
+
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
---
+++ app/src/main/java/com/oashalal/application/MainActivity.java
@@ -0,0 +1,28 @@
+package com.oashalal.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.oashalal.application.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inflate and get instance of binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        // set content view to binding's root
+        setContentView(binding.getRoot());
+        TextView label = findViewById(R.id.label);
+        label.setText("Hello, World");
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.binding = null;
+    }
+}
