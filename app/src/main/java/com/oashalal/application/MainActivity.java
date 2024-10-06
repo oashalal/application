991407cc46b34ddc651d9ecb3d6a2589dbe2ce80
Your Name <you@example.com>
2024-10-06 11:02:09 +0300

    auto

--- app/src/main/java/com/oashalal/application/MainActivity.java
+++ app/src/main/java/com/oashalal/application/MainActivity.java
@@ -10,9 +10,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    
     private ActivityMainBinding binding;
+    
     public TextView label;
-    public int num;
+    public TextView title;
+
+    public int currentDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,30 +29,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         label = findViewById(R.id.label);
-        Button button = findViewById(R.id.button);
+        title = findViewById(R.id.title);
 
-        label.setText("0");
-        label.setTextSize(25);
-        num = 0;
+        label.setTextSize(17);
+        title.setTextSize(25);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        Button nextButton = findViewById(R.id.button_next);
+        Button backButton = findViewById(R.id.button_back);
+        
+        nextButton.setText(">");
+        backButton.setText("<");
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                num += 1;
-                try {
-                    label.setText(Integer.toString(num));
-                } catch (Exception ec) {
-                    label.setText("error");
+                if (currentDay == 7) {
+                    currentDay = 1;
+                    updateSchedule();
+                    return;
                 }
-            }            
+                currentDay += 1;  
+                updateSchedule();
+            }                   
         });
-        button.setText("add 1");
+        
+        backButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if (currentDay == 1) {
+                    currentDay = 7;
+                    updateSchedule();
+                    return;
+                }
+                currentDay -= 1;  
+                updateSchedule();
+            }                   
+        });
+
     }
     
     @Override
     protected void onDestroy() {
         super.onDestroy();
         this.binding = null;
+    }
+
+    private void initSchedule() {
+        currentDay = ScheduleService.getDay();
+        updateSchedule(); 
+    }
+
+    public void updateSchedule() {
+        String[] schedule = ScheduleService.getSchedule(currentDay);
+        String titleString = ScheduleService.getDayName(currentDay);
+        
+        StringBuilder builder = new StringBuilder();
+        for (String string : schedule) {
+            builder.append(string + "\n"); 
+        }
+        title.setText(titleString);
+        label.setText(builder.toString());
     }
 }
---
+++ app/src/main/java/com/oashalal/application/ScheduleService.java
@@ -0,0 +1,84 @@
+package com.oashalal.application;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * ScheduleService
+ */
+public class ScheduleService {
+    
+    private static List<String[]> list;
+
+    {
+        list = new ArrayList<String[]>();
+        list.add(new String[] {
+            "разг. о важном",
+            "литература",
+            "ф - ра",
+            "алгебра",
+            "ОБЗР",
+            "история",
+            "обществознание"
+        });
+        list.add(new String[] {
+            "химия",
+            "ф - ра",
+            "алгебра",
+            "биология",
+            "геометрия",
+            "рус. яз.",
+            "литература"
+        });
+        list.add(new String[] {
+            "физика",
+            "рус. яз.",
+            "геометрия",
+            "информатика",
+            "англ. яз.",
+            "алгебра",
+            "инд. проект"
+        });
+        list.add(new String[] {
+            "горизонты",
+            "литература",
+            "алгебра",
+            "биология",
+            "вероятность",
+            "обществознание",
+            "англ. яз."
+        });
+        list.add(new String[] {
+            "физика",
+            "биология",
+            "англ. яз.",
+            "геометрия",
+            "ф - ра",
+            "география",
+            "история"
+        });
+
+    }
+
+    public static int getDay() {
+        LocalDateTime time = LocalDateTime.now();
+        DayOfWeek dayOfWeek = time.getDayOfWeek();
+        return dayOfWeek.getValue();
+    }
+
+    public static String[] getSchedule(int day) {
+        try {
+            return list.get(day);
+        } catch (Exception ec) {
+            return new String[0];
+        }
+    }
+
+    public static String getDayName(int day) {
+        return DayOfWeek.of(day).getDisplayName(TextStyle.FULL, new Locale("ru"));
+    }
+}
